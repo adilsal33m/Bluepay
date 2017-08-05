ac5894b337ad7fdd96bb62fb981e72dfd6b19577
adilsal33m <adilsaleem.92@gmail.com>
2017-08-05 21:33:55 +0500

    AccountInfo now shows transaction code. Also Final Transaction will be processed n transaction code matching.

--- app/src/main/java/com/app/bluepay/AccountInfo.java
+++ app/src/main/java/com/app/bluepay/AccountInfo.java
@@ -29,7 +29,7 @@ public class AccountInfo extends Fragment {
     public TextView mUsername;
     public TextView mAmount;
     public TextView mLast;
-    private static String ACCOUNT_URL= "http://192.168.1.3:8080/android/account.php";
+    private static String ACCOUNT_URL= "https://bluepay0.000webhostapp.com/android/account.php";
     ProgressDialog pDialog;
     getUserInterface mListener;
 
